16db05a62c0b4cee02ca7d0d791d3e585cbaf943
Natalie Spencer <gnataliespen@gmail.com>
2019-09-11 11:41:58 -0500

    added turn heading

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const board = document.querySelector(".board");
 let turn = 0;
 const button = document.querySelector(".reset");
 const tiles = document.querySelectorAll(".tile");
+const p1p2 = document.querySelector("h2");
 
 board.addEventListener("click", function(evt) {
   console.log(evt.target.className);
@@ -10,9 +11,11 @@ board.addEventListener("click", function(evt) {
       if (turn % 2 === 0) {
         turn++;
         evt.target.style.background = "red";
+        p1p2.innerText = "Its player twos turn";
       } else {
         turn++;
         evt.target.style.background = "blue";
+        p1p2.innerText = "Its player ones turn";
       }
       evt.target.dataset.clicked = "true";
     }
@@ -23,5 +26,7 @@ button.addEventListener("click", function() {
   tiles.forEach(tile => {
     tile.style.background = "white";
     tile.dataset.clicked = "false";
+    p1p2.innerText = "Its player ones turn";
+    turn = 0;
   });
 });
