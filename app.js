49d260067c3cb9d0ab45e84acc2cdcb7ddc80896
Natalie Spencer <gnataliespen@gmail.com>
2019-09-11 12:13:03 -0500

    added gameover

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let turn = 0;
 const button = document.querySelector(".reset");
 const tiles = document.querySelectorAll(".tile");
 const p1p2 = document.querySelector("h2");
+const boardArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
 board.addEventListener("click", function(evt) {
-  console.log(evt.target.className);
   if (evt.target.className.split(" ")[0] === "tile") {
     if (evt.target.dataset.clicked !== "true") {
       if (turn % 2 === 0) {
@@ -20,14 +20,29 @@ board.addEventListener("click", function(evt) {
       evt.target.dataset.clicked = "true";
     }
   }
+  gameOver();
 });
-button.addEventListener("click", function() {
-  console.log(tiles);
+const reset = () => {
   tiles.forEach(tile => {
     tile.style.background = "white";
     tile.dataset.clicked = "false";
     p1p2.innerText = "Its player ones turn";
     turn = 0;
   });
-});
-//test
+};
+
+button.addEventListener("click", reset);
+
+const gameOver = () => {
+  let go;
+  tiles.forEach(tile => {
+    console.log(tile.dataset);
+    if (tile.dataset.clicked !== "true") {
+      go = false;
+    }
+  });
+  console.log(go);
+  if (go !== false) {
+    p1p2.innerText = "Game Over";
+  }
+};
